891f3c92e53915bc2f2a1b1ce0b14df243841566
Eric Barnaba <ericbarnaba@zipcoders-MacBook-Pro-25.local>
2018-03-21 14:55:30 -0400

    constructors for message and id set

--- Client/src/main/java/Id.java
+++ Client/src/main/java/Id.java
@@ -1,2 +1,21 @@
 public class Id {
+    String userid;
+    String name;
+    String githubid;
+
+    public Id(String userid, String name, String githubid){
+        this.userid=userid;
+        this.name=name;
+        this.githubid=githubid;
+    }
+
+    public Id(String name, String githubid){
+        this.userid="-";
+        this.name=name;
+        this.githubid=githubid;
+    }
+
+
+
+
 }
--- Client/src/main/java/Message.java
+++ Client/src/main/java/Message.java
@@ -1,2 +1,33 @@
 public class Message {
+    String sequence;
+    String timestamp;
+    String fromid;
+    String toid;
+    String message;
+
+    public Message(String fromid, String message){
+        this.sequence = "-";
+        this.timestamp="2018-03-21T01:00:00.0Z";
+        this.fromid = fromid;
+        this.toid = "";
+        this.message = message;
+    }
+
+    public Message(String fromid, String toid, String message){
+        this.sequence = "-";
+        this.timestamp="2018-03-21T01:00:00.0Z";
+        this.fromid = fromid;
+        this.toid = toid;
+        this.message = message;
+    }
+
+    public Message(String sequence, String timestamp, String fromid, String toid, String message){
+        this.sequence = sequence;
+        this.timestamp=timestamp;
+        this.fromid = fromid;
+        this.toid = toid;
+        this.message = message;
+    }
+
+
 }
