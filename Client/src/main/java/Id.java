9ebace58e263392bdc720f5231df017455738c6b
Eric Barnaba <ericbarnaba@zipcoders-MacBook-Pro-25.local>
2018-03-21 17:25:57 -0400

    registered with the chat server

--- Client/src/main/java/Id.java
+++ Client/src/main/java/Id.java
@@ -1,18 +1,35 @@
 public class Id {
-    String userid;
-    String name;
-    String githubid;
+    private String userid;
+    private String name;
+    private String github;
 
-    public Id(String userid, String name, String githubid){
+    public Id(String userid, String name, String github){
         this.userid=userid;
         this.name=name;
-        this.githubid=githubid;
+        this.github=github;
     }
 
-    public Id(String name, String githubid){
+    public Id(String name, String github){
         this.userid="-";
         this.name=name;
-        this.githubid=githubid;
+        this.github=github;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGithub() {
+        return github;
+    }
+
+    @Override
+    public String toString(){
+        return "User ID: " + this.userid + "Name: " + this.name +  "GitHub: " + this.github;
     }
 
 
--- Client/src/main/java/YouAreEll.java
+++ Client/src/main/java/YouAreEll.java
@@ -1,10 +1,34 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.fluent.Form;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
 public class YouAreEll {
 
+
+
     YouAreEll() {
+
     }
 
     public static void main(String[] args) {
+        ObjectMapper mapper = new ObjectMapper();
         YouAreEll urlhandler = new YouAreEll();
+//        Id ericB = new Id("EricB", "EricBarnaba");
+//        try {
+//            String json = mapper.writeValueAsString(ericB);
+//            urlhandler.MakeURLCall("/ids", "POST", json );
+//            //System.out.println(json);
+//        }
+//        catch(JsonProcessingException jpe){
+//            System.out.println(jpe.getMessage());
+//        }
+
         System.out.println(urlhandler.MakeURLCall("/ids", "GET", ""));
         System.out.println(urlhandler.MakeURLCall("/messages", "GET", ""));
     }
@@ -18,6 +42,36 @@ public class YouAreEll {
     }
 
     public String MakeURLCall(String mainurl, String method, String jpayload) {
+        String url = "http://zipcode.rocks:8085" + mainurl;
+
+        if(method.equals("GET")){
+            try{
+                System.out.println(Request.Get(url)
+                        .execute().returnContent());
+            }
+            catch(IOException ioe){
+                System.out.println(ioe.getMessage());
+            }
+
+        }
+        else if (method.equals("POST")){
+            try {
+                //StringEntity json = new StringEntity(jpayload);
+                System.out.println(jpayload);
+                Request.Post(url)
+                        .useExpectContinue().bodyString(jpayload, ContentType.DEFAULT_TEXT)
+                        .execute().returnContent();
+            }
+            catch(IOException ioe){
+                System.out.println(ioe.getMessage());
+            }
+
+
+        }
+        else if (method.equals("PUT")){
+
+        }
+
         return "nada";
     }
 }
